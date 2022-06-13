b017f5abe80db50347b4b0ecf75e613821efd4da
Rossiitaa <rosscuotto1998@gmail.com>
2022-06-13 16:16:17 +0200

    exercise finished

---
+++ main.js
@@ -0,0 +1,13 @@
+const userName = prompt ('Inserisci il tuo nome')
+document.getElementById('name-user').innerHTML = userName
+
+const userLastName = prompt ('Inserisci il tuo cognome')
+document.getElementById('lastname-user').innerHTML = userLastName
+
+const userColor = prompt ('Inserisci il tuo colore preferito')
+document.getElementById('color-user').innerHTML = userColor
+
+console.log (userName, userLastName, userColor)
+
+
+
